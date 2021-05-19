27f9d9c701a13205c3a37dcc3ba7f7a72e77e3fe
Peter Bodechtel <sweetlikepete@gmail.com>
2021-05-19 17:01:24 +0200

    fixed some postcss issues

--- postcss-config/src/index.ts
+++ postcss-config/src/index.ts
@@ -1,13 +1,10 @@
 
 
-import autoprefixer from "autoprefixer";
-import postcssCalc from "postcss-calc";
 import postcssColormin from "postcss-colormin";
 import postcssDiscardComments from "postcss-discard-comments";
 import postcssDiscardDuplicates from "postcss-discard-duplicates";
 import postcssDiscardEmpty from "postcss-discard-empty";
 import postcssFontVariant from "postcss-font-variant";
-import postcssImageSetPolyfill from "postcss-image-set-polyfill";
 import postcssMergeLonghand from "postcss-merge-longhand";
 import postcssMergeRules from "postcss-merge-rules";
 import postcssMinifyFontValues from "postcss-minify-font-values";
@@ -18,22 +15,18 @@ import postcssNormalizePositions from "postcss-normalize-positions";
 import postcssNormalizeUrl from "postcss-normalize-url";
 import postcssNormalizeWhitespace from "postcss-normalize-whitespace";
 import postcssOrderedValues from "postcss-ordered-values";
-import postcssPresetEnv from "postcss-preset-env";
 import postcssReduceInitial from "postcss-reduce-initial";
 import postcssReduceTransforms from "postcss-reduce-transforms";
+
 import type { Transformer } from "postcss";
 
+
 const plugins: Transformer[] = [
-    autoprefixer({
-        flexbox: "no-2009"
-    }) as unknown as Transformer,
-    postcssCalc as unknown as Transformer,
     postcssColormin,
     postcssDiscardComments,
     postcssDiscardDuplicates,
     postcssDiscardEmpty,
     postcssFontVariant,
-    postcssImageSetPolyfill,
     postcssMergeLonghand,
     postcssMergeRules,
     postcssMinifyFontValues,
@@ -44,7 +37,6 @@ const plugins: Transformer[] = [
     postcssNormalizeUrl,
     postcssNormalizeWhitespace,
     postcssOrderedValues,
-    postcssPresetEnv,
     postcssReduceInitial,
     postcssReduceTransforms
 ];
